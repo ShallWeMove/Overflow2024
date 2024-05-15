023ec749be385882dbf6ce06a3704095d1b68359
이창준 <joeylee1121@gmail.com>
2024-05-15 16:18:56 +0900

    pnpm

--- frontend/apps/src/components/MoveCallTest.tsx
+++
@@ -1,7 +0,0 @@
-import { useWallet } from "@suiet/wallet-kit";
-
-export const MoveCallTest = () => {
-	const wallet = useWallet();
-	console.log("wallet: ", wallet);
-	return <div>movecall</div>;
-};
