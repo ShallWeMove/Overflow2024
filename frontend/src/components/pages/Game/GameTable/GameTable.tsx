780daa9cb3499769df78e651792b09ae68e373b1
Jarry <jarry@daangn.com>
2024-05-31 23:45:40 +0900

    feat: UI 업데이트

--- frontend/src/components/pages/Game/GameTable/GameTable.tsx
+++ frontend/src/components/pages/Game/GameTable/GameTable.tsx
@@ -1,10 +1,18 @@
-import { Box, styled } from "@mui/material";
-import { tableAtom } from "@/lib/states";
-import { useAtom } from "jotai";
-import { TotalAmount } from "@/components/pages/Game/GameTable/TotalAmount";
+import {Box, styled} from "@mui/material";
+import {tableAtom, userSpaceAtom} from "@/lib/states";
+import {useAtom} from "jotai";
+import {TotalAmount} from "@/components/pages/Game/GameTable/TotalAmount";
+import {convertGameStatusTypeToInt, GameStatusType} from "@/api/game";
+import {SettleUpButton} from "@/components/pages/Game/GamePlayBar/SettleUpButton";
 
-export const GameTable = () => {
+interface GameTableProps {
+	gameTableId: string;
+}
+
+export const GameTable = ({ gameTableId }: GameTableProps) => {
 	const [tableInfo] = useAtom(tableAtom);
+	const [playerInfo] = useAtom(userSpaceAtom);
+
 	return (
 		<Container>
 			<TotalAmount
@@ -17,9 +25,37 @@ export const GameTable = () => {
 				anteAmount={tableInfo?.anteAmount}
 				winnerPlayer={tableInfo?.winnerPlayer}
 			/>
+			{
+				tableInfo?.gamePlayingStatus == convertGameStatusTypeToInt(GameStatusType.GAME_FINISHED) &&
+				tableInfo?.winnerPlayer == playerInfo?.fields.playerAddress ?
+					<WinnerBadge gameTableId={gameTableId} /> :
+					<LoserBadge />
+			}
 		</Container>
 	);
 };
+
+interface WinnerBadgeProps {
+	gameTableId: string;
+}
+
+const WinnerBadge = ({ gameTableId }: WinnerBadgeProps) => {
+	return (
+		<Container>
+			<h1>Win!!</h1>
+			<p>You are the winner! settle up the game to get your reward.</p>
+			<SettleUpButton gameTableId={gameTableId} />
+		</Container>
+	);
+}
+
+const LoserBadge = () => {
+	return (
+		<Container>
+			<h1>Lose</h1>
+		</Container>
+	);
+}
 
 const Container = styled(Box)({
 	display: "flex",
